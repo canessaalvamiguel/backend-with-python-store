d398098303fcd69e54daaf09e4eed024ca8e7f3b
Miguel Canessa <canessaalvamiguel@gmail.com>
2024-04-24 11:38:26 -0500

    Add new Model: Brand

--- products/models.py
+++ products/models.py
@@ -8,7 +8,10 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     sku = models.CharField(max_length=10)
     category = models.CharField(max_length=50)
-    brand = models.CharField(max_length=50)
+    brand = models.ForeignKey(
+        'products.Brand', on_delete=models.CASCADE,
+        related_name='products'
+    )
 
     image = models.ImageField(
         blank=True, null=True,
@@ -26,3 +29,24 @@ class Product(models.Model):
 
     def __str__(self):
         return f'{self.name} | {self.brand}'
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField(null=True, blank=True)
+
+    logo = models.ImageField(
+        blank=True, null=True,
+        upload_to='media/products/'
+    )
+
+    created_at = models.DateTimeField(
+        default=timezone.now
+    )
+    published_at = models.DateTimeField(
+        blank=True, null=True
+    )
+
+    def __str__(self):
+        return self.name
+
