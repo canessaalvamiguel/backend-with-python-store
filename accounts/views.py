d032a004abbd9296fcac0614521d9a8253d37fd7
Miguel Canessa <canessaalvamiguel@gmail.com>
2024-04-28 23:54:26 -0500

    Implement signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import login, logout
 
 
@@ -20,4 +20,17 @@ def logout_view(request):
     if request.method == 'POST':
         logout(request)
         return redirect('index')
+
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'accounts/signup.html', {'form': form})
 
